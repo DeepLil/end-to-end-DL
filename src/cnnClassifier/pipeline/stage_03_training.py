e1783ae2deaba2e33067dddf3cc31a9ea3a9ed01
DeepLil <phanisandeep1994@gmail.com>
2024-05-10 17:51:32 +0530

    training added

--- src/cnnClassifier/pipeline/stage_03_training.py
+++ src/cnnClassifier/pipeline/stage_03_training.py
@@ -1,11 +1,12 @@
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.prepare_callbacks import PrepareCallback
+from cnnClassifier.components.training import Training
 from cnnClassifier import logger
 
 
-STAGE_NAME = "Prepare callback"
+STAGE_NAME = "Training"
 
-class PrepareBaseModelTrainingPipeline:
+class ModelTrainingPipeline:
     def __init__(self):
         pass
 
@@ -13,13 +14,22 @@ class PrepareBaseModelTrainingPipeline:
         config = ConfigurationManager()
         prepare_callback_config = config.get_prepare_callback_config
         prepare_callback = PrepareCallback(config=prepare_callback_config)
-        prepare_callback.get_tb_ckpt_callbacks
+        callback_list = prepare_callback.get_tb_ckpt_callbacks
+
+
+        training_config = config.get_training_config()
+        training = Training(config = training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train(
+            callback_list = callback_list
+        )
 
 
 if __name__ == 'main':
     try:
         logger.info(f">>>> stage {STAGE_NAME} started <<<<\n\nx========x")
-        obj = PrepareCallback()
+        obj = ModelTrainingPipeline()
         obj.main()
         logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nx========x")
     except Exception as e:
